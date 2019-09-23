6cb3e9ff9140b1e3be7f2cf4c5c9b225aceecb01
esritesh <es.ritesh.shrestha@gmail.com>
2019-09-23 15:54:17 +0545

    added form to create new posts

--- resources/js/components/CreateComponent.vue
+++ resources/js/components/CreateComponent.vue
@@ -1,12 +1,32 @@
-// CreateComponent.vue
+// CreatePost.vue
 
 <template>
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card card-default">
-        <div class="card-header">Create Component</div>
-
-        <div class="card-body">I'm the Create Component component.</div>
+  <div>
+    <div class="row">
+      <div class="col-md-8 offset-md-2 bg-white border rounded box-shadow p-5">
+        <h1>Create A Post</h1>
+        <form @submit.prevent="addPost">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Post Title:</label>
+                <input type="text" class="form-control" v-model="post.title" />
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>Post Body:</label>
+                <textarea class="form-control" v-model="post.body" rows="5"></textarea>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="form-group">
+            <button class="btn btn-primary">Create</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -14,8 +34,15 @@
 
 <script>
 export default {
-  mounted() {
-    console.log("Component mounted.");
+  data() {
+    return {
+      post: {}
+    };
+  },
+  methods: {
+    addPost() {
+      console.log(this.post);
+    }
   }
 };
 </script>
