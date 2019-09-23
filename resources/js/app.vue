0e67525d40c4357b573c3f9b5db2dc9302384f44
esritesh <es.ritesh.shrestha@gmail.com>
2019-09-23 15:44:58 +0545

    added navigation bar

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -2,25 +2,36 @@
 
 <template>
   <div class="container">
-    <div>
-      <transition name="fade">
-        <router-view></router-view>
-      </transition>
-    </div>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/create" class="nav-link">Create Post</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+      </ul>
+    </nav><br />
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
 </style>
 
 <script>
-export default {};
+
+    export default{
+    }
 </script>
